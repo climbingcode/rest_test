308f41a1106f4a23a2a524de2abdf065952bec77
wf@widerfunnel.com <thomas.davis@widerfunnel.com>
2019-04-08 19:17:21 -0700

    Completed basic setup

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const config = require('./webpack.config.js');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express(),
+            DIST_DIR = __dirname,
+            HTML_FILE = path.join(DIST_DIR, 'index.html'),
+            compiler = webpack(config);
+
+app.use(express.static(DIST_DIR));
+
+app.use(webpackDevMiddleware(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler)); 
+
+app.get('*', (req, res) => {
+    res.sendFile(HTML_FILE);
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening to ${PORT}....`);
+    console.log('Press Ctrl+C to quit.');
+});
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const mode = process.env.MODE || 'development';
+
+module.exports = {
+  mode,
+  entry: {
+    index: [
+      'webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr',
+      './src/index.js'
+    ]
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  externals: '/node_modules/', // Need this to avoid error when working with Express
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+}

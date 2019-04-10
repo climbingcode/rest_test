b0b4a69350629ac04a8127562f3e617ce26afebc
wf@widerfunnel.com <thomas.davis@widerfunnel.com>
2019-04-09 22:02:26 -0700

    Removed duplicates filtering to better match instructions

--- routes.js
+++ routes.js
@@ -29,12 +29,6 @@ const sumTransactionsBalance = transactions => {
   return transactions.reduce((total, transaction) => total += parseInt(transaction.Amount), 0);
 }
 
-// HACK: Filter out duplicate records (Assuming that transactions with the same data, company, account and amount should only have one record)
-// Would be better if each transaction had a unique ID
-const filterDuplicates = transactions => {
-  return transactions.filter(transaction => JSON.stringify(db.hits).indexOf(JSON.stringify(transaction)) === -1);
-}
-
 // Function to collect missing transactions only if API updates
 const collectMissingTransactions = async (res, req, next, page = db.page) => {
 
@@ -47,10 +41,9 @@ const collectMissingTransactions = async (res, req, next, page = db.page) => {
 
   // NOTE: Update result transactions if page has transactions and totalCount has not been exceeded
   if (transactions && db.hits.length < totalCount) {
-    const filteredTransactions = filterDuplicates(transactions);
-    db.hits = [ ...db.hits, ...filteredTransactions ];
+    db.hits = [ ...db.hits, ...transactions ];
     db.totalCount = db.hits.length;
-    db.balance = db.balance += sumTransactionsBalance(filteredTransactions);
+    db.balance = db.balance += sumTransactionsBalance(transactions);
     db.page = page;
   }
 
